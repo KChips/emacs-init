5845db8b51ab517c531549d868aff71465e9abce
OPChips <k915621674@outlook.com>
2018-11-15 22:39:37 +0800

    update

--- init.el
+++ init.el
@@ -5,19 +5,7 @@
 ;; You may delete these explanatory comments.
 
 (add-to-list 'load-path "~/.emacs.d/elpa")
-
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/"))))
-)
-(package-initialize)
+(require `init-elpa)
 
 ;; theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
@@ -31,8 +19,8 @@
 (require `init-web-mode)
 (require `init-auto-complete)
 (require `init-tabbar)
-(require `init-helm-config)
-(require `init-drag-stuff)
+;; (require `init-helm-config)
+;; (require `init-drag-stuff)
 
 
 
